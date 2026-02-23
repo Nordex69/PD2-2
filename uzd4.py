8caf43b30f36fe1766ecafdb24a713daa916eab7
Nordex69 <166751076+Nordex69@users.noreply.github.com>
2026-02-23 16:21:04 +0200

    Add files via upload

---
+++ uzd4.py
@@ -0,0 +1,16 @@
+partitions = [
+    "System;/;50000;85",
+    "Data;/home;150000;40",
+    "Cache;/tmp;5000;10",
+    "Backup;/mnt/backup;500000;92",
+    "USB-Drive;/media/usb;16000;0",
+    "Logs;/var/log;10000;95",
+    "Database;/var/lib/mysql;80000;70",
+    "Shared;/mnt/shared;200000;1",
+    "Win-System;/mnt/win;100000;90",
+    "Recovery;/recovery;2000;98"
+]
+for p in partitions:
+    data = p.split(";")
+    gb = int(data[2])/ 1024
+    print(f"{data[0]}: {gb: .2f} GB")
